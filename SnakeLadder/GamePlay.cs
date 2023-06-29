a2b1bf1f60c6bcf400b0865c6f2668e9592bde2c
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-06-29 09:24:49 +0530

    [Ritesh]add :Player Position

---
+++ SnakeLadder/GamePlay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class GamePlay
+    {
+        public void Play()
+        {
+            int PlayerPosition = 0;
+            Console.WriteLine("Lets Start The Game");
+            Console.WriteLine($"Player Position Is {PlayerPosition}");
+        }
+        public void PlayerDieRoll()
+        {
+            Random random = new Random();
+            int diceRoll = random.Next(1,7);
+            Console.WriteLine($"The Player Get Number {diceRoll} on Die Roll");
+        }
+      
+    }
+}
